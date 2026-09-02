43c7720c5e3f483e97114d9d26141a0e8375903f
akshattiw <akshat.25005024@kiet.edu>
2026-09-02 18:18:05 +0530

    Initial Version

--- ToDo List/ContentView.swift
+++ ToDo List/ContentView.swift
@@ -7,17 +7,107 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct item{
+    var name: String
+    var completed: Bool
+    var important: Bool
+}
+struct ContentView: View{
+    @State var task = ""
+    @State var itemList: [item] = []
+    @State var indx: Int?
+    var body: some View{
+        
+        NavigationStack{
+            
+            Text("To Do List")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.blue)
+            HStack {
+                TextField("Enter New Task", text: $task)
+                    .frame(width: 300, height: 30)
+                    .padding(10)
+                    .background(.ultraThinMaterial)
+                    .foregroundStyle(.gray)
+                    .clipShape(.rect(cornerRadius: 23))
+                
+                
+                Button(){
+                    addTask()
+                } label: {
+                    Text("+")
+                        .font(.title2)
+                        .padding(18)
+                        .background(.blue)
+                        .foregroundStyle(.white)
+                        .clipShape(.circle)
+                }
+            }
+            List{
+                ForEach(itemList.indices, id: \.self){
+                    index in
+                    HStack{
+                        Button{
+                            itemList[index].completed.toggle()
+                        } label: {
+                            if(itemList[index].completed){
+                                Image(systemName: "checkmark.circle")
+                            }
+                            else{
+                                Image(systemName: "circle")
+                            }
+                        }
+                        Text(itemList[index].name)
+                        if(itemList[index].important){
+                            Image(systemName: "star.fill")
+                        }
+                    }
+                    .swipeActions(edge : .leading){
+                        Button{
+                            itemList[index].important.toggle()
+                        } label: {
+                            Text("Mark Important")
+                        }
+                    }
+                    .swipeActions(edge : .trailing){
+                        Button{
+                            indx = index
+                            removeTask()
+                        } label: {
+                            Text("Delete")
+                        } .tint(.red)
+                    }
+                }
+            }
+            .listStyle(.plain)
+            NavigationLink{
+                settingsView()
+            } label: {
+                Image(systemName: "gear")
+            }
         }
-        .padding()
+    }
+    
+    func addTask(){
+        if(!task.isEmpty){
+            itemList.append(item(name: task, completed: false, important: false))
+            task = " "
+        }
+    }
+    
+    func removeTask(){
+        if let indx{
+            itemList.remove(at: indx)
+        }
+        indx = nil
     }
 }
+
+
+
+
+
 
 #Preview {
     ContentView()
---
+++ ToDo List/settingsView.swift
@@ -0,0 +1,18 @@
+//
+//  settingsView.swift
+//  ToDo List
+//
+//  Created by Akshat Tiwari on 02/09/26.
+//
+
+import SwiftUI
+
+struct settingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    settingsView()
+}
